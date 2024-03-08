92877ca530b853629609f783cebd15467db0f7d3
Dennis Morato <dennisquinter1215@gmail.com>
2024-03-08 08:12:09 -0500

    Se arregla metodo limit

--- src/main/java/org/example/view/ViewStairs.java
+++ src/main/java/org/example/view/ViewStairs.java
@@ -17,32 +17,32 @@ public class ViewStairs {
     public void stairs(){
         do {
             do {
-                System.out.println("Enter a number, this can be positive or negative: ");
+                System.out.println("Enter an integer, this can be positive or negative: ");
                 number = new Scanner(System.in);
                 validateIfItIsNumber = validation.stairsValidationNumber(number);
 
                 if (validateIfItIsNumber) {
-                    numberValidate = number.nextInt();
-
-                    if (validation.stairsLimit(numberValidate)) {
-                        if (numberValidate > 0) {
-                            stairs.climbingStairs(numberValidate);
-                        } else if (numberValidate < 0) {
-                            stairs.goDownTheStairs(numberValidate);
+                    do {
+                        numberValidate = number.nextInt();
+                        if (validation.stairsLimit(numberValidate)) {
+                            if (numberValidate > 0) {
+                                stairs.climbingStairs(numberValidate);
+                            } else if (numberValidate < 0) {
+                                stairs.goDownTheStairs(numberValidate);
+                            } else {
+                                stairs.path();
+                            }
                         } else {
-                            stairs.path();
+                            System.out.println("Number of steps exceeds the limit. Please enter a value between -20 and 20.");
+                            System.out.println("Try again.");
+                            number.nextLine();
                         }
-
-                        System.out.println("Do you want to continue? (Y) or other letters to leave");
-                        number.nextLine(); // Consumir la nueva línea después de nextInt()
-                    } else {
-                        System.out.println("Number of steps exceeds the limit. Please enter a value between -20 and 20.");
-                        System.out.println("Try again.");
-                        number.nextLine();
-                    }
+                    } while (!validation.stairsLimit(numberValidate));
                 }
-            } while (validation.validateExit(number.nextLine()));
+                            System.out.println("Do you want to continue? (Y) or other letters to leave");
+                            number.nextLine();
 
+            } while (validation.validateExit(number.nextLine()));
         } while (!validateIfItIsNumber);
     }
 }
